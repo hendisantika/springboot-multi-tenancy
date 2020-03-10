884f148ab212117db9156bdee38b7a280248eeef
Hendi Santika <hendisantika@gmail.com>
2020-03-10 17:10:24 +0700

    #10 - Add CustomerDto

---
+++ src/main/java/com/hendisantika/springbootmultitenancy/web/dto/CustomerDto.java
@@ -0,0 +1,37 @@
+package com.hendisantika.springbootmultitenancy.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-multi-tenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/03/20
+ * Time: 17.09
+ */
+@Data
+@NoArgsConstructor
+public class CustomerDto {
+    @JsonProperty("id")
+    private final Long id;
+
+    @JsonProperty("firstName")
+    private final String firstName;
+
+    @JsonProperty("lastName")
+    private final String lastName;
+
+    @JsonCreator
+    public CustomerDto(@JsonProperty("id") Long id, @JsonProperty("firstName") String firstName, @JsonProperty(
+            "lastName") String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
