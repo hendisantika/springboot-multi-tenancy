d760e1df7de1a0af7e267d0dfbad743d0ef2e39e
Hendi Santika <hendisantika@gmail.com>
2020-03-10 17:04:51 +0700

    #4 - Add Customer Model

---
+++ src/main/java/com/hendisantika/springbootmultitenancy/model/Customer.java
@@ -0,0 +1,40 @@
+package com.hendisantika.springbootmultitenancy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-multi-tenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/03/20
+ * Time: 17.04
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(schema = "sample", name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+}
