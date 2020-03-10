f3c0fa61c47660dc6cc36e254509197587f26cba
Hendi Santika <hendisantika@gmail.com>
2020-03-10 17:07:24 +0700

    #7 - Add JerseyConfig

---
+++ src/main/java/com/hendisantika/springbootmultitenancy/web/configuration/JerseyConfig.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootmultitenancy.web.configuration;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-multi-tenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/03/20
+ * Time: 17.07
+ */
+@Component
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+
+        // Register the Filters:
+        register(TenantNameFilter.class);
+
+        // Register the Resources:
+        register(CustomerResource.class);
+
+        // Uncomment to disable WADL Generation:
+        //property("jersey.config.server.wadl.disableWadl", true);
+
+        // Uncomment to add Request Tracing:
+        //property("jersey.config.server.tracing.type", "ALL");
+        //property("jersey.config.server.tracing.threshold", "TRACE");
+    }
+}
