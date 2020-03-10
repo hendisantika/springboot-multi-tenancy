386f6520a90129d9230b617f4c2545336f529d2c
Hendi Santika <hendisantika@gmail.com>
2020-03-10 17:12:21 +0700

    #12 - Add getAll API in CustomerController

--- src/main/java/com/hendisantika/springbootmultitenancy/web/controller/CustomerController.java
+++ src/main/java/com/hendisantika/springbootmultitenancy/web/controller/CustomerController.java
@@ -1,10 +1,18 @@
 package com.hendisantika.springbootmultitenancy.web.controller;
 
 import com.hendisantika.springbootmultitenancy.repository.CustomerRepository;
+import com.hendisantika.springbootmultitenancy.web.converter.Converters;
+import com.hendisantika.springbootmultitenancy.web.dto.CustomerDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,5 +32,14 @@ public class CustomerController {
     @Autowired
     public CustomerResource(CustomerRepository repository) {
         this.repository = repository;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<CustomerDto> getAll() {
+        // Return the DTO List:
+        return StreamSupport.stream(repository.findAll().spliterator(), false)
+                .map(Converters::convert)
+                .collect(Collectors.toList());
     }
 }
